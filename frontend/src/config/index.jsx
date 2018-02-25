1ac82232e4fd431a7389b7b910e83a964b29ae18
Noah Manneschmidt <noah@manneschmidt.net>
2018-02-24 20:13:52 -0800

    Update config with minimal explanation of features

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -4,9 +4,17 @@ import { Component, render } from 'preact';
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<div style={{ maxWidth: '37em' }}>
 				<h1>No configuration required!</h1>
-				<p>Go to your live dashboard to control follow buttons in real-time.</p>
+				<p>
+					Go to your live dashboard to control follow buttons in real-time.
+					Queue up buttons for known channels to be able to quickly display
+					them on demand, or display them immediately as the need arises.
+				</p>
+				<p>
+					Use auto display name to use a channel's current display name
+					on the button, or manually input your own.
+				</p>
 			</div>
 		);
 	}
