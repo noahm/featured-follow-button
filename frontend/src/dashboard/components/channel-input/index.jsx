4a1f27153d1d8ec6bea2586e1f558930defadbe7
Noah Manneschmidt <noah@manneschmidt.net>
2018-02-24 17:44:32 -0800

    Add basic validation for channel login

--- frontend/src/dashboard/components/channel-input/index.jsx
+++ frontend/src/dashboard/components/channel-input/index.jsx
@@ -1,56 +1,68 @@
-import './style';
 import { Component } from 'preact';
 
+const LOGIN_REGEX = /^[a-z0-9]\w{0,23}$/;
+
 export class ChannelInput extends Component {
-    state = {
-        pendingChannelName: '',
-        pendingDisplayName: '',
-    };
+  state = {
+    pendingChannelName: '',
+    pendingDisplayName: '',
+  };
 
-    render(props, { pendingChannelName, pendingDisplayName }) {
-        return (
-            <form onSubmit={this.onSubmit}>
-                <input width="15" placeholder="Channel Name" value={pendingChannelName} onChange={this.setChannelName} />
-                <br />
-                <input width="15" placeholder="Display Name (optional)" value={pendingDisplayName} onChange={this.setDisplayName} />
-                <br />
-                <button onClick={this.onClickAdd}>Queue</button>
-                <button onClick={this.onClickActivate}>Activate</button>
-            </form>
-        );
-    }
+  render(props, { pendingChannelName, pendingDisplayName }) {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          width="15"
+          placeholder="Channel Login"
+          value={pendingChannelName}
+          onChange={this.setChannelName}
+          pattern="[a-zA-Z0-9]\w{0,23}"
+        />
+        {this.state.pendingChannelName && !this.isValid() && ' No spaces: "lirik"'}
+        <br />
+        <input width="15" placeholder="Display Name (optional)" value={pendingDisplayName} onChange={this.setDisplayName} />
+        <br />
+        <button onClick={this.onClickAdd}>Queue</button>
+        <button onClick={this.onClickActivate}>Activate</button>
+      </form>
+    );
+  }
 
-    setChannelName = (e) => {
-        this.setState({ pendingChannelName: e.currentTarget.value.toLowerCase() });
-    }
+  setChannelName = (e) => {
+    this.setState({ pendingChannelName: e.currentTarget.value.toLowerCase() });
+  }
 
-    setDisplayName = (e) => {
-        this.setState({ pendingDisplayName: e.currentTarget.value });
-    }
+  setDisplayName = (e) => {
+    this.setState({ pendingDisplayName: e.currentTarget.value });
+  }
 
-    onSubmit = (e) => {
-        e.preventDefault();
-    }
+  onSubmit = (e) => {
+    e.preventDefault();
+  }
 
-    onClickAdd = () => {
-        if (!this.state.pendingChannelName) {
-            return;
-        }
-        this.props.onAdd(this.state.pendingChannelName, this.state.pendingDisplayName);
-        this.setState({
-            pendingChannelName: '',
-            pendingDisplayName: '',
-        });
+  onClickAdd = () => {
+    if (!this.isValid()) {
+      return;
     }
+    this.props.onAdd(this.state.pendingChannelName, this.state.pendingDisplayName);
+    this.setState({
+      pendingChannelName: '',
+      pendingDisplayName: '',
+    });
+  }
 
-    onClickActivate = () => {
-        if (!this.state.pendingChannelName) {
-            return;
-        }
-        this.props.onActivate(this.state.pendingChannelName, this.state.pendingDisplayName);
-        this.setState({
-            pendingChannelName: '',
-            pendingDisplayName: '',
-        });
+  onClickActivate = () => {
+    if (!this.isValid()) {
+      return;
     }
+    this.props.onActivate(this.state.pendingChannelName, this.state.pendingDisplayName);
+    this.setState({
+      pendingChannelName: '',
+      pendingDisplayName: '',
+    });
+  }
+
+  isValid() {
+    return LOGIN_REGEX.test(this.state.pendingChannelName);
+  }
 }
