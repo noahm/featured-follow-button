bd6e536629a3299ef326881ddb49ba5537756ee5
Noah Manneschmidt <nmannesc@twitch.tv>
2020-06-19 16:43:25 -0700

    Fix clobbering my on classnames on body element

--- frontend/src/common-styles.ts
+++ frontend/src/common-styles.ts
@@ -1,28 +1,39 @@
 import commonStyles from "./common-styles.css";
-import classNames from "classnames";
 
 /**
  * Call once at app start to apply the right theme classname to the html body tag
  */
 export function applyThemeClass(force?: "dark" | "light") {
   if (force) {
-    document.body.className = commonStyles[force];
+    document.body.className = `${document.body.className} ${commonStyles[force]}`;
     return;
   }
 
   if (typeof Twitch !== "undefined" && Twitch.ext) {
     Twitch.ext.onContext((context, changedProperties) => {
       if (changedProperties.indexOf("theme") >= 0) {
-        document.body.className =
-          context.theme === "dark" ? commonStyles.dark : commonStyles.light;
+        const names = document.body.className.split(" ").filter((name) => {
+          if (name === commonStyles.light && context.theme === "dark") {
+            return false;
+          }
+          if (name === commonStyles.dark && context.theme !== "dark") {
+            return false;
+          }
+          return true;
+        });
+        names.push(
+          context.theme === "dark" ? commonStyles.dark : commonStyles.light
+        );
+        document.body.className = names.join(" ");
       }
     });
   }
 }
 
+/**
+ * Call once to add the classname to set the transparent background color
+ * for overlay mode
+ */
 export function setTransparentBg() {
-  document.body.className = classNames(
-    document.body.className,
-    commonStyles.transparentBg
-  );
+  document.body.className = `${document.body.className} ${commonStyles.transparentBg}`;
 }
